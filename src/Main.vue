9299cc5ea6cf79ab4dc6d173f6464bba9538ffbb
Dexdot <dexdotapb@gmail.com>
2019-10-04 01:03:15 +0300

    Icons width/height

--- src/Main.vue
+++ src/Main.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <router-link class="logo" to="/" @mouseenter.native="hover($refs.logo)">
+    <router-link
+      class="logo ui-btn"
+      to="/"
+      @mouseenter.native="hover($refs.logo)"
+    >
       <svg width="17" height="15" viewBox="0 0 17 15" fill="none" ref="logo">
         <path
           d="M8.54923 2.78119L14.7613 13.793H14.7791L7.88757 1.55152L1.25903 13.793H1.35169L7.23469 2.79592L7.87856 1.59234L8.54923 2.78119Z"
@@ -15,7 +19,11 @@
       </svg>
     </router-link>
 
-    <router-link class="play" to="/" @mouseenter.native="hover($refs.play)">
+    <router-link
+      class="play ui-btn"
+      to="/"
+      @mouseenter.native="hover($refs.play)"
+    >
       <svg width="14" height="17" viewBox="0 0 14 17" fill="none" ref="play">
         <path
           d="M1.2 1.87975L12.6 8.46155L1.2 15.0433L1.2 1.87975Z"
@@ -30,7 +38,7 @@
       </svg>
     </router-link>
 
-    <button class="wave" @mouseenter="hover($refs.wave)">
+    <button class="wave ui-btn" @mouseenter="hover($refs.wave)">
       <svg width="22" height="12" viewBox="0 0 22 12" fill="none" ref="wave">
         <path
           d="M1 7.2623L2.28173 3.6062C3.5467 -0.00206464 8.62677 -0.0653153 9.98119 3.51034L11.642 7.89497C13.3586 12.4267 19.909 11.9838 21 7.2623V7.2623"
@@ -49,7 +57,11 @@
       </svg>
     </button>
 
-    <button class="circles" @mouseenter="mouseenter" @mouseleave="mouseout">
+    <button
+      class="circles ui-btn"
+      @mouseenter="mouseenter"
+      @mouseleave="mouseout"
+    >
       <div class="circles__i" ref="circle1"></div>
       <div class="circles__i" ref="circle2"></div>
       <div class="circles__i"></div>
@@ -126,13 +138,16 @@ export default {
       tl.add({
         targets: c1,
         duration: 350,
-        translateX: ['0px', '10px'],
-        scaleY: [1, 0.3],
+        // translateX: ['0px', '10px'],
+        // scaleY: [1, 0.3],
+        translateX: '10px',
+        scaleY: 0.3,
         easing: 'easeInCirc'
       }).add({
         targets: c2,
         duration: 400,
-        scale: [0, 1],
+        // scale: [0, 1],
+        scale: 1,
         easing: 'easeOutCirc'
       })
     },
@@ -156,13 +171,16 @@ export default {
         duration: 300,
         translateX: '10px',
         translateY: '-10px',
-        scale: [1, 0],
+        // scale: [1, 0],
+        scale: 0,
         easing: 'easeInCirc'
       }).add({
         targets: c1,
         duration: 250,
-        translateX: ['10px', '0px'],
-        scaleY: [0.3, 1],
+        // translateX: ['10px', '0px'],
+        // scaleY: [0.3, 1],
+        translateX: '0px',
+        scaleY: 1,
         easing: 'easeOutCirc'
       })
     }
@@ -171,42 +189,47 @@ export default {
 </script>
 
 <style lang="sass" scoped>
-.logo
+.ui-btn
+  width: 80px
+  height: 80px
+  display: flex
   position: fixed
-  top: var(--unit-v)
-  left: var(--unit-h)
 
-  width: 24px
-  height: 24px
+.logo
+  top: 0
+  left: 0
+
+  padding-top: var(--unit-v)
+  padding-left: var(--unit-h)
 
 .play
   position: fixed
-  top: calc(var(--vh, 1vh) * 100 - var(--unit-v) - 24px)
-  left: var(--unit-h)
+  top: calc(var(--vh, 1vh) * 100 - 80px)
+  left: 0
 
-  display: flex
+  padding-left: var(--unit-h)
+  padding-bottom: var(--unit-v)
   align-items: flex-end
-  width: 24px
-  height: 24px
 
 .wave
   position: fixed
-  top: calc(var(--vh, 1vh) * 100 - var(--unit-v) - 24px)
-  right: var(--unit-h)
+  top: calc(var(--vh, 1vh) * 100 - 80px)
+  right: 0
 
-  width: 24px
-  height: 24px
+  padding-right: var(--unit-h)
+  padding-bottom: calc(var(--unit-v) - 22px)
+  align-items: flex-start
+  justify-content: flex-end
 
 .circles
   position: fixed
-  top: var(--unit-v)
-  right: var(--unit-h)
 
-  display: flex
-  // align-items: center
+  top: 0
+  right: 0
+
   justify-content: flex-end
-  width: 24px
-  height: 24px
+  padding-top: calc(var(--unit-v) - 26px)
+  padding-right: var(--unit-h)
 
   &__i
     width: 4px
@@ -218,11 +241,11 @@ export default {
 
     &:nth-child(1)
       position: absolute
-      right: 10px
+      right: calc(var(--unit-h) + 10px)
 
     &:nth-child(2)
       position: absolute
-      right: 0
+      right: var(--unit-h)
 
       width: 24px
       height: 24px
